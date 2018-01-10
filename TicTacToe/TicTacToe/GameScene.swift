16dfdbd9942cb75bed15b4073a856fdb973bacda
Janie Clayton <janie.k.clayton@gmail.com>
2018-01-09 20:55:05 -0600

    Initial Commit
    This is my initial commit with my first game project. This isn't very far along yet. I have only set up my state objects thus far.

---
+++ TicTacToe/TicTacToe/GameScene.swift
@@ -0,0 +1,24 @@
+//
+//  GameScene.swift
+//  TicTacToe
+//
+//  Created by Janie Clayton on 1/9/18.
+//  Copyright © 2018 Janie Clayton. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+    // Properties
+    
+    // Function Overrides
+    override func didMove(to view: SKView) {
+        
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+    }
+}
---
+++ TicTacToe/TicTacToe/StateUtils.swift
@@ -0,0 +1,21 @@
+//
+//  StateUtils.swift
+//  TicTacToe
+//
+//  Created by Janie Clayton on 1/9/18.
+//  Copyright © 2018 Janie Clayton. All rights reserved.
+//
+
+import Foundation
+
+enum GameState {
+    case NotPlaying
+    case PlayerA
+    case PlayerB
+}
+
+enum BoardState {
+    case NotSelected
+    case PlayerA
+    case PlayerB
+}
